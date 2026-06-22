2ee4cb3c7ad308047e4ae035946531b4f47167db
jotagep <jotagepweb@gmail.com>
2026-06-22 20:01:35 +0200

    chore: add image remote pattern to next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'prueba-tecnica-api-tienda-moviles.onrender.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
