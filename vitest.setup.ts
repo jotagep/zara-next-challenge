da9867dc986c757176023aef2c0ad65561c52534
jotagep <jotagepweb@gmail.com>
2026-06-24 16:24:31 +0200

    test: setup environment with missing browser globals

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,49 @@
 import '@testing-library/jest-dom/vitest'
+
+class ResizeObserverStub {
+  private callback: ResizeObserverCallback
+  constructor(callback: ResizeObserverCallback) {
+    this.callback = callback
+  }
+  observe(target: Element) {
+    this.callback(
+      [
+        {
+          target,
+          contentRect: {
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            toJSON: () => ({}),
+          },
+          borderBoxSize: [],
+          contentBoxSize: [],
+          devicePixelContentBoxSize: [],
+        },
+      ],
+      this
+    )
+  }
+  unobserve() {}
+  disconnect() {}
+}
+
+if (typeof globalThis.ResizeObserver === 'undefined') {
+  globalThis.ResizeObserver = ResizeObserverStub as unknown as typeof ResizeObserver
+}
+
+if (typeof globalThis.PointerEvent === 'undefined') {
+  class PointerEventStub extends MouseEvent {
+    pointerId: number
+    constructor(type: string, init: PointerEventInit = {}) {
+      super(type, init)
+      this.pointerId = init.pointerId ?? 0
+    }
+  }
+  globalThis.PointerEvent = PointerEventStub as unknown as typeof PointerEvent
+}
